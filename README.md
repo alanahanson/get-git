75c4c85a7e6628865e8db8402056af9ecd0b3613
Alana Hanson <ahanson@leapfrogonline.com>
2018-09-03 14:30:20 -0500

    add empty files

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # get_git
 Get user information from Github and Bitbucket profiles
+
+GH
+The user profile should include following information (when avaialble):
+total number of public repos (seperate by original repos vs forked repos)
+- GET /users/:username/repos
+total watcher/follower count
+total number of stars recieved
+total number of stars given
+  - GET /users/:username/starred (array)
+total number of open issues
+total number of commits to their repos (not forks)
+total size of their accounts
+a list/count of langagues used
+a list/count of repo topics
+
+
+
+BB
+The user profile should include following information (when avaialble):
+total number of public repos (seperate by original repos vs forked repos)
+total watcher/follower count
+total number of stars recieved
+total number of stars given
+total number of open issues
+total number of commits to their repos (not forks)
+total size of their accounts
+a list/count of langagues used
+a list/count of repo topics
---
+++ get_git/bitbucket_client.py
@@ -0,0 +1,49 @@
+import requests
+
+BB_BASE_URL='https://api.bitbucket.org/1.0'
+
+class BitbucketClient:
+
+    def __init__(self, username):
+        self.username = username
+        self.user = self._user()
+        self.repos = self._repos()
+
+    def _user(self):
+        return requests.get(f'{GH_BASE_URL}/users/{self.username}').json()
+
+    def _repos(self):
+        return requests.get(f'{GH_BASE_URL}/users/{self.username}/repos').json()
+
+    def orig_repo_number(self):
+        return len([r for r in self.repos if r['fork']])
+
+    def forks_number(self):
+        return len([r for r in self.repos if not r['fork']])
+
+    def follower_count(self):
+        return self.user['followers']
+
+    def stars_received(self):
+        return sum([r['stargazers_count'] for r in self.repos])
+
+    def stars_given(self):
+        return requests.get(f'{GH_BASE_URL}/users/{self.username}/starred')
+
+    def open_issues_count(self):
+        return sum([r['open_issues_count'] for r in self.repos])
+
+    def total_commits(self):
+        # come back to this one - individual get requests for each repo??? :(
+        return
+
+    def account_size(self):
+        return sum([r['size'] for r in self.repos])
+
+    def languages_used(self):
+        # come back to this one - individual get requests for each repo??? :(
+        return
+
+    def repo_topics(self):
+        # ????
+        return
